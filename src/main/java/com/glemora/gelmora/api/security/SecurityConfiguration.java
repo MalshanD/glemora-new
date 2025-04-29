69565be3b532cc2293a1addac6b8013fdeda9983
MalshanD <malshanshalinda115@gmail.com>
2025-04-29 21:29:45 +0530

    create security

---
+++ src/main/java/com/glemora/gelmora/api/security/SecurityConfiguration.java
@@ -0,0 +1,53 @@
+package com.glemora.gelmora.api.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(jsr250Enabled = true) // Enable @RolesAllowed
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+    private final CorsFilter corsFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(corsConfigurer -> {})
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers(antMatchers(HttpMethod.POST, "/api/auth/sign-up")).permitAll();
+                    auth.requestMatchers(antMatchers(HttpMethod.POST, "/api/auth/sign-in")).permitAll();
+                    auth.requestMatchers("/error/**").permitAll();
+                    auth.requestMatchers("/api/auth/**").permitAll();
+                    // Allow OPTIONS requests for CORS preflight
+                    auth.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+
+    private RequestMatcher antMatchers(HttpMethod httpMethod, String antPattern) {
+        return new AntPathRequestMatcher(antPattern, httpMethod.name());
+    }
+}
